920e7c7c6850254ba7131cfef514608ff6f1b1fa
Yesra Fatima <fyesra0@gmail.com>
2025-05-11 20:30:41 +0530

    home page created and the image crousel added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:AIRA/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
   runApp(const MyApp());
 }
 
@@ -13,12 +18,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(),
+      theme: ThemeData(fontFamily: GoogleFonts.poppins().fontFamily),
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
-
-
-
